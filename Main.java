7fd68dba4304a340157be9925fe43991ac67db7e
Mobin Alami <alamimobin85@gmail.com>
2025-12-08 21:46:13 +0330

    p103

--- Main.java
+++ Main.java
@@ -1,10 +1,12 @@
-import java.util.Scanner;//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main
 {
     public static void main(String[] args)
     {
-        Scanner input = new Scanner(System.in);        
+        Scanner input = new Scanner(System.in);
         int s=0;
         int A=1;
         for(int x=1;x<=100;x++)
