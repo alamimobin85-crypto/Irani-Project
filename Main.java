4b0fc41f52be0aceb526fd29b835753921796396
Mobin Alami <alamimobin85@gmail.com>
2025-12-08 21:20:46 +0330

    p102

--- Main.java
+++ Main.java
@@ -5,16 +5,11 @@ public class Main
     public static void main(String[] args)
     {
         int s=0;
-        int A=1;
         for(int x=1;x<=100;x++)
-        {
-            s=(A*x)+s;
-            A=-A;
-
-        }
+            s=s+x;
         System.out.println(s);
 
 
-
     }//end of main
+
 }//end of class
