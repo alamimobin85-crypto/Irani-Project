746fcb377dbffbb49266fe8dfc62e213586d0e09
Mobin Alami <alamimobin85@gmail.com>
2025-12-08 21:04:55 +0330

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int s=0;
+        for(int x=1;x<=100;x++)
+            s=s+x;
+        System.out.println(s);
+
+
+    }//end of main
+
+}//end of class
