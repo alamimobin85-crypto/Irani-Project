8cc0da2afb401767c73d62fb5c7f9a32d5f36e23
Mobin Alami <alamimobin85@gmail.com>
2025-12-09 20:17:09 +0330

    p75

--- Main.java
+++ Main.java
@@ -1,21 +1,28 @@
-import java.util.Scanner;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+import java.util.Scanner;//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main
 {
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
-        int s=0;
-        int A=1;
-        for(int x=1;x<=100;x++)
-        {
-            s=(A*x)+s;
-            A=-A;
 
-        }
-        System.out.println(s);
+        int c=0;
+        for(int a=1;a<=7;a++)
+            for(int b=1;b<=7;b++)
+            {
+                c=a*b;
+                int s=0;
+                int p=1;
+                while (c>0)
+                {
+                    int d=c%8;
+                    s=(d*p)+s;
+                    p=p*10;
+                    c=c/8;
+
+                }//end of while
+                System.out.println(s);
+            }//end of for2
 
 
 
