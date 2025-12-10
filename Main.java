236755eaf378b41437ae70c8164840020ef63177
Mobin Alami <alamimobin85@gmail.com>
2025-12-10 13:31:56 +0330

    p24

--- Main.java
+++ Main.java
@@ -7,15 +7,15 @@ public class Main
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
-        int s=0;
-        int A=1;
-        for(int x=1;x<=100;x++)
-        {
-            s=(A*x)+s;
-            A=-A;
-
-        }
-        System.out.println(s);
+        int n=input.nextInt();
+        int c=0;
+        for(int x=1;x<=n;x++)
+            if(n%x==0)
+                c++;
+        if(c==2)
+            System.out.println("YES");
+        else
+            System.out.println("NO");
 
 
 
