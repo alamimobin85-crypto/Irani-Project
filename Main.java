3972d79c02e46a2c6a9ddd7a371546fb62722a05
Mobin Alami <alamimobin85@gmail.com>
2025-12-10 13:30:00 +0330

    p16

--- Main.java
+++ Main.java
@@ -7,17 +7,12 @@ public class Main
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
-        int s=0;
-        int A=1;
-        for(int x=1;x<=100;x++)
-        {
-            s=(A*x)+s;
-            A=-A;
-
-        }
-        System.out.println(s);
-
-
+        int n=input.nextInt();
+        int p=0;
+        for(int x=1;x<=n;x++)
+            if(n%x==0)
+                p=p+1;
+        System.out.println(p);
 
     }//end of main
 }//end of class
