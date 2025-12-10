a51253feea34c39811864e262a400a9b2bac645f
Mobin Alami <alamimobin85@gmail.com>
2025-12-10 13:44:12 +0330

    p81

--- Main.java
+++ Main.java
@@ -7,16 +7,23 @@ public class Main
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        int n=input.nextInt();
         int s=0;
-        int A=1;
-        for(int x=1;x<=100;x++)
+        int w=1;
+        int d=0;
+        while(n>0)
         {
-            s=(A*x)+s;
-            A=-A;
+            d=n%10;
+            if(d!=0)
+            {
+                s=(d*w)+s;
+                w=w*10;
+            }
 
-        }
-        System.out.println(s);
+            n=n/10;
 
+        }//end of while
+        System.out.print(s);
 
 
     }//end of main
